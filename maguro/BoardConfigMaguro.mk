7289cf11f1b2dbd5625137c5db6055e834972e42
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-07-26 12:31:24 -0400

    maguro: update proprietary binaries
    Change-Id: I849fcef28480d0b419f6e1fed020f0e752ae1720

--- maguro/BoardConfigMaguro.mk
+++ maguro/BoardConfigMaguro.mk
@@ -11,15 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-LOCAL_STEM := maguro/device-maguro.mk
-
-$(call inherit-product, vendor/samsung/maguro/device-vendor-blobs.mk)
-
-$(call inherit-product-if-exists, vendor/broadcom/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/csr/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/imgtec/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/invensense/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/nxp/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/samsung/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/ti/$(LOCAL_STEM))
